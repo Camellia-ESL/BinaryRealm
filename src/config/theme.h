7cbdbe642ec6e85971f9bc08fb2c14d07284e2f2
Camellia-ESL <85369525+Camellia-ESL@users.noreply.github.com>
2025-02-20 17:28:47 +0100

    more about themes

--- src/config/theme.h
+++ src/config/theme.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../external/imgui/imgui.h"
+#include "../../external/nhlomannjson/json.hpp"
 #include "../core/containers.h"
 #include "../core/string.h"
 
@@ -18,13 +19,16 @@ struct RTheme {
 class RThemeManager {
 public:
   // Create's a new theme and returns it in a result
-  RResult<RTheme &> create_theme(const std::string &name);
+  RResult<std::shared_ptr<RTheme>> create_theme(const std::string &name);
 
   // Saves the current state of the theme manager in the theme configs
   bool save();
 
   // Loads the themes from the theme configs
   bool load();
+
+  // Load the default theme in a theme
+  void load_default_theme(RTheme &theme);
 
 private:
   std::shared_ptr<RTheme> selected_theme_;
