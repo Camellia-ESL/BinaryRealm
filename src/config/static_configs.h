6ed776df4a1acfa725591473f7ef3cff162c6f66
Camellia-ESL <85369525+Camellia-ESL@users.noreply.github.com>
2025-05-15 15:30:55 +0200

    lowered resource usage

--- src/config/static_configs.h
+++ src/config/static_configs.h
@@ -7,5 +7,5 @@ constexpr const char* APP_NAME = "BinaryRealm";
 constexpr const char* APP_VERSION = "V.0.1.0 - Beta";
 
 constexpr const float APP_BACKGROUND_MAX_FPS = 240.0f;
-constexpr const float APP_BACKGROUND_LOW_USAGE_FPS = 1.0f;
+constexpr const float APP_BACKGROUND_LOW_USAGE_FPS = 0.0f;
 }  // namespace RStaticConfigs
--- src/core/time.cpp
+++ src/core/time.cpp
@@ -1,12 +1,6 @@
 #include "time.h"
 
-#include "assert.h"
-
 bool RFrameRateLimiter::should_update() {
-  r_assert(
-      framerate_ > 0.0f,
-      "Framerate limit is set to 0.0f, did you forget the initialization?");
-
   auto now = std::chrono::steady_clock::now();
   auto interval = std::chrono::milliseconds((int)(1000.0f / framerate_));
 
